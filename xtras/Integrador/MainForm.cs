e9790a33ad3e7a33e2a22b7bb96964134bcc63a1
Gerardo Tordoya <zherar7ordoya@outlook.com>
2025-03-11 20:03:32 -0300

    Integrador

---
+++ xtras/Integrador/MainForm.cs
@@ -0,0 +1,10 @@
+namespace Integrador
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ xtras/Integrador/Persona.cs
@@ -0,0 +1,35 @@
+﻿namespace Integrador;
+
+public class Persona(string dni,
+                     string nombre,
+                     string apellido)
+{
+    public string DNI { get; init; } = dni ?? throw new ArgumentNullException(nameof(dni));
+    public string Nombre { get; set; } = nombre ?? throw new ArgumentNullException(nameof(nombre));
+    public string Apellido { get; set; } = apellido ?? throw new ArgumentNullException(nameof(apellido));
+
+    private readonly List<Auto> autos = [];
+
+    public List<Auto> ObtenerAutos() => autos;
+    public int CantidadAutos => autos.Count;
+
+    public void AgregarAuto(Auto auto)
+    {
+        if (auto.Dueño != null)
+        {
+            throw new InvalidOperationException("El auto ya tiene un dueño.");
+        }
+        autos.Add(auto);
+        auto.AsignarDueño(this);
+    }
+
+    public void EliminarAuto(Auto auto)
+    {
+        if (autos.Remove(auto))
+        {
+            auto.EliminarDueño();
+        }
+    }
+
+    ~Persona() => Console.WriteLine($"El objeto Persona con DNI {DNI} ha sido eliminado.");
+}
