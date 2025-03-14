7e2e3bb510e1b87231faad7c2f2e1071f9eb35bf
Gerardo Tordoya <zherar7ordoya@outlook.com>
2025-03-13 23:32:41 -0300

    Update Entity.cs

--- xtras/Integrador/Abstract/Entity.cs
+++ xtras/Integrador/Abstract/Entity.cs
@@ -1,6 +1,9 @@
-﻿namespace Integrador.Abstract;
+﻿using System.Xml.Serialization;
+
+namespace Integrador.Abstract;
 
 public abstract class Entity : IEntity
 {
+    [XmlAttribute]
     public int Id { get; set; }
 }
